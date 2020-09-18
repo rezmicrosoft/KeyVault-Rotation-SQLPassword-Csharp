bef1c24876474af9808846f162e1144e3b6c3319
Renpeng Zhang <rez@microsoft.com>
2020-09-18 14:27:59 -0700

    script

--- scripts/rez-test.sh
+++ scripts/rez-test.sh
@@ -1,5 +1,6 @@
 kv_name='rez-sqlrotation-kv'
 sql_server_resource_id='/subscriptions/27b750cd-ed43-42fd-9044-8d75e124ae55/resourceGroups/rez-test-packer/providers/Microsoft.Sql/servers/reztest'
+sql_admin_pwd='34754262-b59b-4324-8ea8-4e033826dfe3'
 
 az account set -s 27b750cd-ed43-42fd-9044-8d75e124ae55
 
@@ -7,4 +8,5 @@ az group deployment create -g rez-test --template-file ../arm-templates/azuredep
 
 expire_on=$(date -u --date='tomorrow' '+%Y-%m-%dT%H:%M:%SZ')
 
-az keyvault secret set --name sqlAdminPassword --vault-name rez-sqlrotation-kv --value "Simple123" --tags "CredentialId=rez" "ProviderAddress=$sql_server_resource_id" "ValidityPeriodDays=90" --expires $expire_on
+az keyvault secret set --name sqlAdminPassword --vault-name rez-sqlrotation-kv --value $sql_admin_pwd --tags "CredentialId=rez" "ProviderAddress=$sql_server_resource_id" "ValidityPeriodDays=90" --expires $expire_on
+
